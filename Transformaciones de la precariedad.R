8dbdf5aac8e303cc56b8f80d4c2903eaa97a1377
gasparh440 <tucorreo@gmail.com.ar>
2025-02-08 18:22:06 -0300

    borre el cuadro de desocupados

--- Transformaciones de la precariedad.R
+++ Transformaciones de la precariedad.R
@@ -18,11 +18,3 @@ C_ocup_catocup_registro<- base %>% filter(ESTADO==1) %>%    summarise(Ocupados  
                                                               Cuenta_propia = sum (PONDERA[CAT_OCUP==2]),
                                                               Patron = sum (PONDERA[CAT_OCUP==1]),
                                                               Trafam = sum (PONDERA[CAT_OCUP==4]))
-C_desocup_catocup_registro<- base %>% filter(ESTADO==3) %>%  summarise(Ocupados    = sum(PONDERA[ESTADO == 1]),
-                                                                      Asal_protegidos = sum (PONDERA[CAT_OCUP==3& PP07H==1]),
-                                                                      Asal_precariosTot = sum (PONDERA[CAT_OCUP==3& PP07H==2]),
-                                                                      Asal_precarios_i1_mono = sum (PONDERA[CAT_OCUP==3& PP07I==1]),
-                                                                      Asal_precarios_i2_negr = sum (PONDERA[CAT_OCUP==3& PP07I==2]),
-                                                                      Cuenta_propia = sum (PONDERA[CAT_OCUP==2]),
-                                                                      Patron = sum (PONDERA[CAT_OCUP==1]), 
-                                                                      Trafam = sum (PONDERA[CAT_OCUP==4]))
